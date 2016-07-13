32fe1f1ffdd3229a7c8e711efcbcc821c82c43df
manofthelionarmy <aleon45@ucmerced.edu>
2016-07-12 18:17:10 -0700

    Added queryHandler

--- webpage.go
+++ webpage.go
@@ -3,7 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-	"github.com/UCMAndesLab/goSMAP"
+	"github.com/UCMAndesLab/gosMAP"
 	"github.com/gin-gonic/gin"
 	"html/template"
 	//"io/ioutil"
@@ -13,9 +13,11 @@ import (
 	//"time"
 )
 
-var responseTemplate = template.Must(template.ParseFiles("view.html", "add.html"))
+var responseTemplate = template.Must(template.ParseFiles("view.html", "query.html"))
+var apikey string = "rU3eqtaE4zBSzZKjoUS9Q7fVPbTmKmD2eOUr"
 
-var validPath = regexp.MustCompile("^/(add|save|view)/([a-zA-Z0-9]+)$")
+var validPath = regexp.MustCompile("^/(query|save|view)/([a-zA-Z0-9]+)$")
+var uuid string
 
 type Page struct {
 	Title    string
@@ -27,44 +29,32 @@ type Page struct {
 }*/
 
 func createPage(title string) (*Page, error) {
-	key := "rU3eqtaE4zBSzZKjoUS9Q7fVPbTmKmD2eOUr"
-	uuid := "51427e0d-ee71-5df2-90b5-ebc3cc720f87"
-	conn, e := gosMAP.Connect("http://mercury:8079", key)
+
+	//uuid := "51427e0d-ee71-5df2-90b5-ebc3cc720f87"
+	conn, e := gosMAP.Connect("http://mercury:8079", apikey)
+	if e != nil {
+		return nil, e
+	}
 
 	d, err := conn.Get(uuid, 0, 0, 10)
-	if e != nil {
-		panic(err)
+	fmt.Println(uuid)
+	if err != nil {
+		return nil, err
 	}
 
 	return &Page{Title: title, ReadData: d[0].Readings}, nil
 }
 
-func exampleQuery(c *gin.Context) []gosMAP.Data {
-	key := "rU3eqtaE4zBSzZKjoUS9Q7fVPbTmKmD2eOUr"
-	uuid := "51427e0d-ee71-5df2-90b5-ebc3cc720f87"
-
-	conn, err := gosMAP.Connect("http://mercury:8079", key)
-
-	if err != nil {
-		panic(err)
-	}
-
-	q := conn.Query(fmt.Sprintf("select * where uuid='%s'", uuid))
-
-	/*for _, r := range q[0].Readings {
-		fmt.Printf("%s", r.Time.String())
-	}*/
-
-	return q
-}
-
 func viewHandler(c *gin.Context) {
 
-	title, err := getTitle(c)
+	title := c.Request.URL.Path
+	//query := c.Request.FormValue("query")
 
-	if err != nil {
+	fmt.Println(title)
+
+	/*if err != nil {
 		return
-	}
+	}*/
 
 	p, err := createPage(title)
 
@@ -75,15 +65,36 @@ func viewHandler(c *gin.Context) {
 
 	//responseTemplate.Execute(c.Writer, p)
 	renderTemplate(c, "view", p)
+	//fmt.Fprintf(c.Writer, "<h1>Something</h1>")
 }
-func addHandler(c *gin.Context) {
+func queryHandler(c *gin.Context) {
 	//title := c.Request.FormValue("title")
-	renderTemplate(c, "add", nil)
+	renderTemplate(c, "query", nil)
 }
 func saveHandler(c *gin.Context) {
-	title := c.Request.FormValue("title")
 
-	c.Redirect(http.StatusFound, "/view/"+title)
+	conn, err := gosMAP.Connect("http://mercury:8079", apikey)
+
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, errors.New("Cannot connect to the server"))
+	}
+
+	query := c.Request.FormValue("query")
+
+	//fmt.Println(query)
+
+	//d, err := conn.QueryList("select distinct uuid where Metadata/Location/Building = 'Facilities A'")
+	d, err := conn.QueryList(fmt.Sprintf("select distinct uuid where Metadata/Location/Building = '%s'", query))
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+	}
+
+	uuid = d[0]
+	for i := range d {
+		fmt.Println(d[i])
+	}
+
+	c.Redirect(http.StatusFound, "/view/"+query)
 }
 
 func renderTemplate(c *gin.Context, tmpl string, p *Page) {
@@ -108,8 +119,8 @@ func getTitle(c *gin.Context) (string, error) {
 func main() {
 
 	router := gin.Default()
-	router.GET("/view/:title", viewHandler)
-	router.GET("/add/", addHandler) //ok, for a new page with no title yet, don't include :adding in the url
+	router.GET("/view/:query", viewHandler)
+	router.GET("/query/", queryHandler) //ok, for a new page with no title yet, don't include :adding in the url
 	router.POST("/save/", saveHandler)
 	router.Run(":8080")
 	//createPage("TestData")
